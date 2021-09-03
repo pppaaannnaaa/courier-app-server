cee4475887b99266237295d8d585c0f7e9e912f9
pppaaannnaaa <pppaaannnaaa@gmail.com>
2021-09-03 20:46:45 +0530

    first commit

---
+++ mocks/agents.js
@@ -0,0 +1,44 @@
+const AGENTS = {
+    agent1:{
+        password: 'test',
+        fullName: 'agent1',
+    },
+    agent2:{
+        password: 'test',
+        fullName: 'agent2',
+    },
+    agent3:{
+        password: 'test',
+        fullName: 'agent3',
+    },
+    agent4:{
+        password: 'test',
+        fullName: 'agent4',
+    },
+    agent5:{
+        password: 'test',
+        fullName: 'agent5',
+    },
+    agent6:{
+        password: 'test',
+        fullName: 'agent6',
+    },
+    agent7:{
+        password: 'test',
+        fullName: 'agent7',
+    },
+    agent8:{
+        password: 'test',
+        fullName: 'agent8',
+    },
+    agent9:{
+        password: 'test',
+        fullName: 'agent9',
+    },
+    agent10:{
+        password: 'test',
+        fullName: 'agent10',
+    }
+};
+
+export default AGENTS;
---
+++ mocks/users.js
@@ -0,0 +1,24 @@
+const USERS = {
+    user1:{
+        password: 'test',
+        fullName: 'User1',
+    },
+    user2:{
+        password: 'test',
+        fullName: 'User2',
+    },
+    user3:{
+        password: 'test',
+        fullName: 'User3',
+    },
+    user4:{
+        password: 'test',
+        fullName: 'User4',
+    },
+    user5:{
+        password: 'test',
+        fullName: 'User5',
+    }
+};
+
+export default USERS;
